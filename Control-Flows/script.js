33d801ad42c38fe305f4fa6d179840c07a2a003a
Damilare <da9783790@gmil.com>
2025-02-26 16:03:12 +0100

    first commit

---
+++ Control-Flows/script.js
@@ -0,0 +1,288 @@
+console.log("Hello world!")
+
+// control flows... determines how programmes are executed based on conditions and iteration.
+
+// conditionals -- if, else-if, else, ternary operator.
+
+if ("condition") {
+    //code to execute if condition is met
+}
+
+let age = 30;
+
+if (age === 25) {
+    console.log("age is greater than 25") 
+}else if (age < 25) {
+    console.log("age is greater than 25")
+} else {
+    console.log("Age is 30")
+}
+
+
+let isLoggedIn = true;
+
+if (!isLoggedIn) {
+    console.log("You are not logged in")
+} else {
+    console.log("You are logged in.")
+}
+
+let balance = 500;
+let withdrawalAmount = 1000;
+
+if (withdrawalAmount > balance) {
+    console.log("withdrawalAmount is above the balance")
+} else if  (withdrawalAmount == balance) {
+    console.log("You can not empty your wallet!")
+} else {
+    console.log("successful..")
+}
+
+let num = 18;
+
+if (num < 18) {
+    console.log("You are less than 18 you can't vote")
+} else  {
+    console.log("vote successfully")
+}
+
+let purchaseAmount = 100;
+let discount;
+
+
+if (purchaseAmount >= 250) {
+   discount = 10
+} else if (purchaseAmount >= 150) {
+    discount = 5
+}  else if (purchaseAmount === 100) {
+    discount = 1.5
+}else {
+    discount = 0
+}
+
+console.log(`You got a discount of ${discount}`)
+
+
+// Ternary operator.
+// condition ? expression_if_true : expression_if_false.
+
+let threashold = 40; 
+
+threashold >= 40 ? console.log("Marry") : console.log("You are still a minor");
+
+const number = 9;
+
+const result = number % 2 === 0 ? "even" : "odd"
+
+console.log(result)
+
+let temperature = 100
+
+const entry = temperature < 20 ? "it is a cool day" : "It is a hot day";
+
+console.log(entry);
+
+
+
+// Switch statment -- used when there a multiple possibl value for a variable.
+
+let day ="Tuesday"
+
+switch(day) {
+    case "monday":
+        console.lod("Today is bright!")
+        break;
+        case "Friday":
+            console.log("Jimoh Oloyin")
+            break;
+            case "Tuesday":
+                console.log("Today is Tuesday");
+                break;
+                default:
+                    console.log("It is a normal day");
+}
+
+    let order = "pizza"
+
+    switch(order) {
+        case "Burger":
+        console.log("This is burger");
+        break;
+
+        case "pizzer":
+            console.log("You ordered pizzer ");
+            break;
+
+            case "Doughnut":
+                console.log("This is Doughut");
+                break;
+
+                default:
+                    console.log("invalid order.... please select from the menu");
+    }
+
+
+    let dayNumber = 4;
+    let dayName;
+
+
+    switch(dayNumber) {
+        case 1:
+        dayName = "Sunday";
+        break;
+
+        case 2:
+        dayName = "Monday";
+        break;
+
+        case 3:
+        dayName = "Tuesday";
+        break;
+
+        case 4:
+        dayName = "Friday";
+        break
+
+        default:
+            dayName = "not available "
+        } 
+            console.log(`day ${dayNumber} is ${dayName}`)
+
+
+            
+            let rool = "Admin"
+
+            switch(rool) {
+                case "Admin":
+                console.log("Welcome admin");
+                break;
+        
+                case "Editor":
+                    console.log("welcome editor");
+                    break;
+        
+                    case "Viewer":
+                        console.log("Welcome Viewer");
+                        break;
+        
+                        default:
+                            console.log("unknow rool please contact support");
+            }
+
+
+
+            //Loops are used to execute a block of code multiple times.
+            
+            // for (initialization; condition; update) {
+             //  // code execute
+           // }
+
+            for(let i = 0; i < 10; i++) {
+                console.log(i)
+            }
+
+            let table = 5;
+            for (let i = 1; i <= 10; i++) {
+                console.log(`${table} * ${i} = ${table * i}`)
+            }
+        
+        
+            // Example2
+
+            for (let i = 1; i < 10; i+=2) {
+                console.log(i)
+            }
+
+            for(let i = 0; i <= 5; i++) {
+                if(i === 3) {
+                    continue
+                }
+                console.log(i)
+            }
+           
+    let fruit = ["apple", "orange", "mango", "apricot"];
+for (let i = 1; i < fruit.length; i++){
+        console.log(fruit[i])
+    }
+
+
+   let car = "Damilare";
+   for (let i = 0; i < car.length; i++){
+    console.log(car[i])
+   }
+
+
+   //for...in loop
+
+   //Example
+
+
+   let user ={name: "Damilare", age: 15, city: "ogun"} 
+
+   for(key in user) {
+    console.log(key + ":", user [key])
+   }
+
+   //for of loop
+
+   let colours = ["Blue", "Red", "Yello", "Pink"]
+
+   for(colour of colours){
+    console.log(colour);
+   }
+
+   // while loop
+
+   // while (condition) {
+    // // code to execute
+  // }
+
+  // Example
+
+ // let i = 1;
+
+//   while(i <= 7) {
+//     console.log(i)
+//     i++;
+//   }
+
+
+let y = 0;
+
+while(y < 5) {
+    y++;
+
+    if(y === 3) {
+        continue;
+    }
+
+    console.log(y)
+}
+
+let fruits = ["apple", "banana", "cherry"];
+let i = 0;
+
+while  (i < fruits.length) {
+  console.log(fruits[i]);
+  i++;
+}
+
+// foreach loop 
+
+// Example
+const student = ['Alabi', 'Mubarak', 'Habeeb']
+student.forEach((student,i) =>{
+    console.log(`student ${i}: ${student}`)
+})
+
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(function(number) {
+  console.log(number);
+});
+
+
+
+
+
+
---
+++ Introduction to javascript/script.js
@@ -0,0 +1,114 @@
+//  Javascript Variables, Data Types, and Operators
+
+// variable Keywords: const and let
+
+let a = 10;
+console.log(a)
+
+
+let b = 3;
+let c = 4;
+let d;
+
+
+d = b;
+b = c;
+c = d;
+
+console.log(b)
+console.log(c)
+
+// Javascript Data types
+
+let studentNames = ["Damilare", "Feranmi", "Dammy"] // non primitive
+
+//primitive Data types
+let primitive= 10; // primitive
+let anotherPrimitive = primitive;
+
+primitive = 30;
+
+console.log({primitive, anotherPrimitive})
+
+
+//Non primitive
+
+
+let newStudentNames = studentNames;
+
+
+studentNames = ["Soliu", "Mueez"];
+
+console.log({studentNames, newStudentNames})
+
+
+//  Javascript operators
+
+//logical operator
+
+const num1 = 0;
+
+const name1 = 0;
+
+// const trueValue = num1 $$ name1 ? "It is truthy value" : "It is false"
+const trueValue = num1 || name1 ? "It is truthy value" : "It is false"
+
+console.log(`${trueValue}`)
+
+
+
+// comparision operator
+
+let num2 = 10;
+// let num3 = 10;
+
+// const result = num2 == num3 ? "yes": "no"
+
+console.log(num2); 
+
+
+
+
+
+
+
+
+// Equality operator
+// strict
+
+console.log(5 === "5")
+console.log(5 === 5)
+console.log(true === 1)
+console.log([] === 0)
+console.log(null === undefined)
+// console.log([1, 2,3] === [1, 2, 3])
+// console.log({} === {})
+
+
+let obj1 = {name: "lateefa"};
+let obj2 = {name: "lateefa"};
+
+console.log(obj1 === obj2)
+
+let array1 = [1, 2,3] 
+console.log(typeof array1)
+let array2 = [1, 2,3]
+
+console.log(array1 === array2)
+
+
+// loose
+
+console.log(5 == "5");
+console.log(5 == 5);
+console.log(true == 1);
+console.log(null == undefined);
+console.log(null == false)
+console.log([] == 1);
+console.log([1] == 1);
+console.log("\t\n" == 0);
+console.log("damilare" == "dare")
+console.log([1, 2] == "1, 2")
+
+
+
